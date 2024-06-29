5153c41d354e84c3e5a271ef350fcf777ea29743
meiezbr <mayezbr@outlook.fr>
2024-06-29 16:41:59 +0100

    some updates

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ mongoose.connect(`mongodb+srv://${user}:${password}@${host}/${database}?retryWri
 });
 
 /* import the models */
+// from Models
 
 // import Student model
 const StudentModel = require("./models/Student")
